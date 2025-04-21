0484f242d5c7bf391e3a8a56dff9166f58ad7f06
Muhammad Aji Massaid <ajimassaid.jobs@gmail.com>
2025-04-21 21:50:46 +0700

    fix userdropdown

--- frontend/src/components/header/UserDropdown.tsx
+++ frontend/src/components/header/UserDropdown.tsx
@@ -32,7 +32,7 @@ export default function UserDropdown() {
           <img src="/images/user/owner.jpg" alt="User" />
         </span>
 
-        <span className="block mr-1 font-medium text-theme-sm">  {user || "-"}</span>
+        <span className="block mr-1 font-medium text-theme-sm">  {user['full_name'] || "-"}</span>
         <svg
           className={`stroke-gray-500 dark:stroke-gray-400 transition-transform duration-200 ${
             isOpen ? "rotate-180" : ""
